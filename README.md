54cae103a5e433d8491bcfe0b4078c524635bf10
Siva Jr <sivarohit36@gmail.com>
2023-10-28 20:06:32 +0530

    scroll to top

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Detailed and underrated Concepts or Features of Frontend techonolgy
 
 1) Meta Tags
-2) Very simple password visiblity using HTML, CSS and JS
+2) Very simple password visiblity
 3) Post data to Api and Get data from Api using JavaScript Fetch Api
+4) Scroll to top
---
+++ scroll top/main.js
@@ -0,0 +1,17 @@
+function scrollUp(){
+
+    const scrollUp = document.getElementById('scroll-up')
+
+    if(this.scrollY >= 560){
+
+        scrollUp.classList.add('show-scroll')
+
+    }else{
+
+        scrollUp.classList.remove('show-scroll')
+
+    }
+
+}
+
+window.addEventListener('scroll', scrollUp)
